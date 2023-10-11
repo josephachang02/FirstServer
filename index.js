3b7dc94aea9ecabf48798b803203ef469a755ade
Joe Chang <131396136+josephachang02@users.noreply.github.com>
2023-10-10 19:45:08 -0700

    first server

---
+++ index.js
@@ -0,0 +1,9 @@
+//Type out an express server that has 3 routes. Make requests to your server using Thunderclient OR the browser.
+
+ 
+
+// GET "/":   sends text "Welcome to my server, make a request to '/info' to learn more! "
+
+// - GET "/info": sends text  "This server will send you data about the creator of this server if you make a GET request to '/data'  "
+
+// - GET "/data": sends an object that describes you.
